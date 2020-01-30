252d669f1afb646706d6546c04ef13524a810788
liviasilvasantos <liviasilvasantos@gmail.com>
2020-01-30 10:24:53 -0300

    Usando data binding - Two-way data binding

---
+++ data-binding/src/app/meu-form/meu-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-form',
+  templateUrl: './meu-form.component.html',
+  styleUrls: ['./meu-form.component.css']
+})
+export class MeuFormComponent implements OnInit {
+
+  nome:string = "ABC";
+
+  pessoa:any = {
+    nome: 'Lívia',
+    idade: 37
+  };
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
