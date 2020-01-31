f57be4ff1472efb0902a80242c1b341bb0356cd1
liviasilvasantos <liviasilvasantos@gmail.com>
2020-01-31 17:12:15 -0300

    Diretivas: usando ngIf.

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -1,3 +1,4 @@
+import { CursoDetalheComponent } from './../../../../primeiro-projeto/src/app/cursos/curso-detalhe/curso-detalhe.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgifComponent implements OnInit {
 
+  cursos: string[] = ['Angular'];
+
+  mostrarCursos: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onMostrarCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
+  }
 }
